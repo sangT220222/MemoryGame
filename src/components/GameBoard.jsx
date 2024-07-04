40e76a8c2f635e4047989af4d16b219cc88ef064
sangT220222 <magnas9@hotmail.co.uk>
2024-07-04 17:17:29 +0100

    update paths

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import GetPokemonImages from "./FetchAPI";
-import Card from "./Card";
+import Card from "../components/Card.jsx";
 import PropTypes from "prop-types";
 import "../styles/gameBoard.css";
 
